204ad521ae65f461b2a6a7694e10acb441027f90
AgborSuccess <agborsuccess2007@gmail.com>
2023-01-02 23:46:34 -0800

    Made the TTS to work and Stopped the app from crashing, and I've also Implemented the settings fragment to pass data

--- app/src/main/java/com/example/pronouncetwo/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/pronouncetwo/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,12 @@
 package com.example.pronouncetwo.ui.notifications;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,8 @@ import com.example.pronouncetwo.databinding.FragmentNotificationsBinding;
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private SeekBar seekBarPitch;
+    private SeekBar seekBarSpeed;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +31,21 @@ public class NotificationsFragment extends Fragment {
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+
+
+        seekBarPitch = binding.pitchSeekBar;
+        seekBarSpeed = binding.speedSeekBar;
+
+        notificationsViewModel.setSeekBarPitch(seekBarPitch.getProgress());
+        notificationsViewModel.setSeekBarSpeed(seekBarSpeed.getProgress());
+
+
+
+
+
         return root;
     }
 
@@ -34,4 +54,41 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Get the shared preferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("SEEKBAR_PREFERENCES", Context.MODE_PRIVATE);
+
+        // Get the editor
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // Store the values of the seekBar in the shared preferences
+        editor.putInt("seekBarPitch", seekBarPitch.getProgress());
+        editor.putInt("seekBarSpeed", seekBarSpeed.getProgress());
+
+        // Commit the changes
+        editor.apply();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Get the shared preferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("SEEKBAR_PREFERENCES", Context.MODE_PRIVATE);
+
+        // Retrieve the values of the seekBar from the shared preferences
+        int pitch = sharedPreferences.getInt("seekBarPitch", 0);
+        int speed = sharedPreferences.getInt("seekBarSpeed", 0);
+
+        // Set the values of the seekBar
+        seekBarPitch.setProgress(pitch);
+        seekBarSpeed.setProgress(speed);
+    }
+
+
 }
--- app/src/main/java/com/example/pronouncetwo/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/pronouncetwo/ui/notifications/NotificationsViewModel.java
@@ -1,5 +1,7 @@
 package com.example.pronouncetwo.ui.notifications;
 
+import android.widget.SeekBar;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
